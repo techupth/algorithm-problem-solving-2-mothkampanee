5c8f1f75412b2bb80e8ce152ce0edc498b330455
Moth <kampanee.moth@gmail.com>
2024-07-17 15:43:50 +0700

    feat: algorithm problem solving search data return index in array

--- ex-1.js
+++ ex-1.js
@@ -1,5 +1,51 @@
+let students = [
+  "Andrew",
+  "Bobby",
+  "Diana",
+  "George",
+  "Hannah",
+  "Isaac",
+  "Jasmine",
+  "John",
+];
+
 function findStudentIndex(students, searchStudent) {
-	// เริ่มเขียนโค้ดตรงนี้จ้า
+  // เริ่มเขียนโค้ดตรงนี้จ้า
+  // Binary Search
+  let left = 0;
+  let right = students.length - 1;
+  let index = -1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    if (students[mid] === searchStudent) {
+      return (index = mid);
+    } else if (students[mid] < searchStudent) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  //   Linear Search
+  //   let index = -1;
+  //   for (let i = 0; i <= students.length; i++) {
+  //     if (students[i] === searchStudent) {
+  //       return (index = i);
+  //     }
+  //   }
+
+  return index;
 }
 
-// ตอบคำถามตรงนี้จ้า
+const result = findStudentIndex(students, "Jasmine");
+console.log(result);
+
+/* 
+ตอบคำถามตรงนี้จ้า 
+Binary Search = O(n)
+	เพราะมีการตรวจสอบสมาชิกภายใน array ทุกตัวแต่จะตัดสมาชิกภายใน array ฝั่งซ้ายมือออกครึ่งนึง
+	ถ้าพบว่าสมาชิกภายใน array ตำแหน่งตรงกลางมีค่าน้อยกว่าตำแหน่งซ้ายสุด
+	จะทำการขยับตำแหน่งซ้ายสุดมาอยู่ในตำแหน่งที่มากกว่าตำแหน่งตรงกลางหนึ่งตำแหน่ง
+	และนำตำแหน่งซ้ายมาบวกกับตำแหน่งขาวหารด้วยสอง จะได้ตำแหน่งตรงการใหม่
+*/
--- ex-2.js
+++ ex-2.js
@@ -1,3 +1,42 @@
-function findBookIndex(books, searchTitle) {
+let books = [
+  { title: "A Tale of Two Cities", author: "Charles Dickens" },
+  { title: "Brave New World", author: "Aldous Huxley" },
+  { title: "Catch-22", author: "Joseph Heller" },
+  { title: "Don Quixote", author: "Miguel de Cervantes" },
+  { title: "Ender's Game", author: "Orson Scott Card" },
+  { title: "Fahrenheit 451", author: "Ray Bradbury" },
+  { title: "Gone with the Wind", author: "Margaret Mitchell" },
+  { title: "Harry Potter", author: "J.K. Rowling" },
+];
+
+function findBookIndex(books, searchBook) {
   // เริ่มเขียนโค้ดตรงนี้จ้า
+  // Binary Search
+  let left = 0;
+  let right = books.length - 1;
+  let index = -1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    if (books[mid].title === searchBook) {
+      return (index = mid);
+    } else if (books[mid].title < searchBook) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  // Linear Search
+  // let index = -1;
+  // for (let key = 0; key <= books.length; key++) {
+  //   if (books[key].title === searchBook) {
+  //     return (index = key);
+  //   }
+  // }
+
+  return index;
 }
+
+const result = findBookIndex(books, "A Tale of Two Cities");
+console.log(result);
